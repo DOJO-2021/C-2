954c617370b0541704c3a96dda3c3621819bdf65
DOJO2021-h-onoda <onoda@seplus2016.onmicrosoft.com>
2021-06-14 11:49:07 +0900

    更新/小野田

--- Komike/src/model/Test_choice_detail.java
+++ Komike/src/model/Test_choice_detail.java
@@ -1,5 +1,39 @@
 package model;
 
 public class Test_choice_detail {
+	private int choice_number;
+	private boolean true_false;
+	private String choice;
 
+	//コンストラクタの生成
+		public Test_choice_detail(int choice_number, boolean true_false, String choice) {
+			super();
+			this.choice_number = choice_number;
+			this.true_false = true_false;
+			this. choice =   choice;
+		}
+
+		public int getChoice_number() {
+			return choice_number;
+		}
+
+		public void setChoice_number(int choice_number) {
+			this.choice_number = choice_number;
+		}
+
+		public boolean isTrue_false() {
+			return true_false;
+		}
+
+		public void setTrue_false(boolean true_false) {
+			this.true_false = true_false;
+		}
+
+		public String getChoice() {
+			return choice;
+		}
+
+		public void setChoice(String choice) {
+			this.choice = choice;
+		}
 }
