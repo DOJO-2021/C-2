9b49cd8a3fd71683eb973fd33dbe31dc522ac447
DOJO2021-h-onoda <onoda@seplus2016.onmicrosoft.com>
2021-06-22 15:13:00 +0900

    更新/小野田

--- Komike/src/servlet/Test.java
+++ Komike/src/servlet/Test.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import dao.TestsDao;
 import dao.TestsresultDao;
+import model.Select;
 import model.Test_question;
 import model.Test_result;
 
@@ -36,6 +37,11 @@ public class Test extends HttpServlet {
 		//List<Test_question> questions =TDao.select();
 		List<Test_question> questions =TDao.select(key);
 
+		//Tselect.jspのジャンル名がTest.jspの問題番号に使う
+		request.setAttribute("Select",
+				new Select(key));
+
+
 
 
 		// 検索結果をリクエストスコープに格納する
@@ -58,17 +64,48 @@ public class Test extends HttpServlet {
 		// リクエストパラメータを取得する
 		//■requestスコープからジャンルとIDを取得
 		request.setCharacterEncoding("UTF-8");
-		String question_number = request.getParameter("question_number");
-		//Int choice_number = request.getParameter("choice_number");
+		//String question_number = request.getParameter("question_number");
+		//String choice_number = request.getParameter("choice_number");
 		//boolean true_false = request.getParameter("GENRE");
+
+		//javaやHTMLなど、なんの問題かを取得
+		//String testType = "java" 固定するためのもの
+		String testType = request.getParameter("testType");
+
+		//正解数を保持する変数
+		int score = 0;
+		//ループ２０回
+
+		//for(){}
+		//対応する問題番号の選択した値を取得
+		int answer = Integer.parseInt(request.getParameter(testType + "01")) ;
+		//正解か間違いかを判断
+		TestsresultDao resultDao = new TestsresultDao();
+		//正解していたら＋１
+		if (resultDao.select(testType + "01", answer)) {
+			//正解
+			score ++;
+			System.out.println("正解!");
+			request.setAttribute("Test_result",
+					new Test_result(1, 15,  2,49, "reborn","CSS"));
+		}else {
+			//不正解
+			System.out.println("不正解・・・");
+			request.setAttribute("Test_result",
+					new Test_result(1, 14,  2,44, "reborn","CSS"));
+		}
+
+
+		//正解数が取得できる
+
 
 		// 選ばれたラジオボタンの情報を取得
 		//String s = request.getParameter("radiobutton");
 
 
 		//Daoからデータを持ってくる
-		TestsresultDao dao = new TestsresultDao();
-		if (dao.select("question_number", 2)) {
+		/*TestsresultDao dao = new TestsresultDao();
+		if (dao.select("question_number", 0)) {
 			//それがもしtrueなら正解になる
 			System.out.println("正解!");
 			request.setAttribute("Test_result",
@@ -78,26 +115,8 @@ public class Test extends HttpServlet {
 			System.out.println("不正解・・・");
 			request.setAttribute("Test_result",
 					new Test_result(1, 14,  2,44, "reborn","CSS"));
-		}
+		}*/
 
-
-
-		/*request.setAttribute("Test_result",
-				new Test_result(1, 15,  2,49, "reborn","CSS"));*/
-
-		//それぞれのラジオボタンから抜き出した選択肢を正誤フラグと比べる
-		//そこからあっていた数だけ正解数correct_answerに代入する
-
-
-		//(正解数/20)×100のパーセンテージにして正解数correct_answer_rateに代入する
-
-		//正解数に応じてランクの数字を決める
-
-		// 採点結果をリクエストスコープに格納する
-		//request.setAttribute("Test_question",
-		//new Test_choice ("Java", 11));
-
-		//ArrayList<Test_question> questions = new ArrayList<Test_question>();
 
 		if(false) {
 			//エラーメッセージの出力
